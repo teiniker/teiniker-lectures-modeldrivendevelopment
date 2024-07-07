f2a66be8c0a69bea0df9b347565d431f2c45486c
teiniker <egon.teiniker@gmail.com>
2024-07-07 21:40:43 +0200

    Refactorings: moved examples.

---
+++ metamodeling/dsl/intern/AnnotationProcessing-Entity/src/main/java/org/se/lab/metamodel/MEntity.java
@@ -0,0 +1,30 @@
+package org.se.lab.metamodel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MEntity 
+	extends MNamedElement
+{
+	/*
+	 * Constructor
+	 */
+	public MEntity(String name)
+	{
+		setName(name);
+	}
+	
+	
+	/*
+	 * Reference: ---[*]-> properties:MProperty[*]
+	 */
+	private List<MProperty> properties = new ArrayList<MProperty>();
+	public List<MProperty> getProperties()
+	{
+		return properties;
+	}
+	public void setProperties(List<MProperty> properties)
+	{
+		this.properties = properties;
+	}	
+}
